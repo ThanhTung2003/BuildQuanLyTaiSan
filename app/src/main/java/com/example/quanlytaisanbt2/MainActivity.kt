af9fa1f47c5d19242e4ae50a8a9aa03783f6468d
ThanhTung2003 <tungb100203@gmail.com>
2024-09-17 10:43:20 +0700

    update binding không xài findviewbyid nữa

--- app/src/main/java/com/example/quanlytaisanbt2/MainActivity.kt
+++ app/src/main/java/com/example/quanlytaisanbt2/MainActivity.kt
@@ -5,64 +5,46 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.EditText
-import android.widget.LinearLayout
-import android.widget.ListView
-import android.widget.RadioGroup
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.quanlytaisanbt2.UI.formatMoney
+import com.example.quanlytaisanbt2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     @SuppressLint("MissingInflatedId", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         Log.d("baitap2", "onCreate")
 
         val personList = mutableListOf<Person>()
         val assetList = mutableListOf<Asset>()
-        val totalAssetsList = mutableListOf<Asset>()
         val currentPersonAssets = mutableListOf<Asset>()
-
-        val radioGroupPeopleAndAsset = findViewById<RadioGroup>(R.id.radioGroupPeopleAndAsset)
-        val layoutPeople = findViewById<LinearLayout>(R.id.layoutPeople)
-        val layoutAssets = findViewById<LinearLayout>(R.id.layoutAssets)
-        val editTextPeople = findViewById<EditText>(R.id.editTextPeople)
-        val buttonAddPeople = findViewById<Button>(R.id.buttonAddPeople)
-        val editTextAsset = findViewById<EditText>(R.id.editTextAsset)
-        val editTextValueAsset = findViewById<EditText>(R.id.editTextValueAsset)
-        val buttonAddAsset = findViewById<Button>(R.id.buttonAddAsset)
-        val peopleListView = findViewById<ListView>(R.id.peopleListView)
-        val assetListView = findViewById<ListView>(R.id.assetListView)
-        val textListAssets = findViewById<TextView>(R.id.textListAssets)
-        val personResult = findViewById<TextView>(R.id.personResult)
-        val assetResult = findViewById<TextView>(R.id.assetResults)
 
         val peopleAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, personList.map { it.getPersonName() })
         val assetAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, assetList.map { it.getInfo() })
 
-        peopleListView.adapter = peopleAdapter
-        assetListView.adapter = assetAdapter
+        binding.peopleListView.adapter = peopleAdapter
+        binding.assetListView.adapter = assetAdapter
 
-        layoutPeople.visibility = View.VISIBLE
-        layoutAssets.visibility = View.GONE
+        binding.layoutPeople.visibility = View.VISIBLE
+        binding.layoutAssets.visibility = View.GONE
         Log.d("baitap2", "View mặc định")
 
-        radioGroupPeopleAndAsset.setOnCheckedChangeListener { _, checkid ->
+        binding.radioGroupPeopleAndAsset.setOnCheckedChangeListener { _, checkid ->
             when (checkid) {
                 R.id.radiobuttonPeople -> {
-                    layoutPeople.visibility = View.VISIBLE
-                    layoutAssets.visibility = View.GONE
+                    binding.layoutPeople.visibility = View.VISIBLE
+                    binding.layoutAssets.visibility = View.GONE
                     Log.d("baitap2", "View Con người")
 
                 }
                 R.id.radiobuttonAsset -> {
-                    layoutPeople.visibility = View.GONE
-                    layoutAssets.visibility = View.VISIBLE
+                    binding.layoutPeople.visibility = View.GONE
+                    binding.layoutAssets.visibility = View.VISIBLE
                     Log.d("baitap2", "View Tài sản")
 
                 }
@@ -72,7 +54,7 @@ class MainActivity : AppCompatActivity() {
         fun updateTextListAssets() {
 
             val assetsInfo = currentPersonAssets.joinToString(", ") { it.getName() }
-            textListAssets.text = assetsInfo
+            binding.textListAssets.text = assetsInfo
         }
 
         fun addAssetToCurrentPerson(asset: Asset) {
@@ -109,20 +91,20 @@ class MainActivity : AppCompatActivity() {
 
             val assetsInfo = assetList.map { asset -> "${asset.getName()} - ${asset.value.formatMoney()}" }
             val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, assetsInfo)
-            assetListView.adapter = adapter
+            binding.assetListView.adapter = adapter
         }
 
-        buttonAddAsset.setOnClickListener {
-            val assetName = editTextAsset.text.toString()
-            val assetValue = editTextValueAsset.text.toString().toLongOrNull()
+        binding.buttonAddAsset.setOnClickListener {
+            val assetName = binding.editTextAsset.text.toString()
+            val assetValue = binding.editTextValueAsset.text.toString().toLongOrNull()
 
             if (assetName.isNotEmpty() && assetValue != null) {
                 val asset = Asset(assetName, assetValue)
                 assetList.add(asset)
                 assetAdapter.notifyDataSetChanged()
                 addAssetToCurrentPerson(asset) // add tài sản vào người hiện tại
-                editTextAsset.text.clear()
-                editTextValueAsset.text.clear()
+                binding.editTextAsset.text.clear()
+                binding.editTextValueAsset.text.clear()
                 updateAssetsListView()
                 Log.d("baitap2", "Thêm $assetName thành công với giá: ${assetValue.formatMoney()}")
             } else {
@@ -130,31 +112,34 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        buttonAddPeople.setOnClickListener {
-            val personName = editTextPeople.text.toString()
+        binding.buttonAddPeople.setOnClickListener {
+            val personName = binding.editTextPeople.text.toString()
             savePerson(personName)
-            editTextPeople.text.clear()
+            binding.editTextPeople.text.clear()
         }
 
         fun viewResult() {
             val builder = StringBuilder()
             builder.append("- Thống kê danh sách đối tượng:\n")
 
-            // Thêm tất cả "Con người"
+
             personList.forEach { person ->
                 builder.append(person.getInfoStatistical()).append("\n")
             }
 
-            // Thêm tất cả "Tài sản"
+
             assetList.forEach { asset ->
                 builder.append(asset.getInfoStatistical()).append("\n")
             }
 
-            // Hiển thị kết quả trên Logcat
             Log.d("baitap2", builder.toString())
         }
 
-        personResult.setOnClickListener {
+        binding.personResult.setOnClickListener {
+            viewResult()
+        }
+
+        binding.assetResults.setOnClickListener {
             viewResult()
         }
     }
